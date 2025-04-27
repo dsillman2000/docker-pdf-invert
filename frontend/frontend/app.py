c85b949ca81c9ae40c4c24ec3489e01f92fd44f7
dsillman2000 <dsillman2000@yahoo.com>
2025-04-27 09:43:18 -0400

    fix: updating frontend to use flask-wtf for form management

--- frontend/frontend/app.py
+++ frontend/frontend/app.py
@@ -1,15 +1,40 @@
 from pathlib import Path
 import flask
+import flask_wtf
+import wtforms
+import os
 import jinja2
 
 
 TEMPLATES_DIR = Path(__file__).parent.parent / "templates"
 app = flask.Flask(__name__, template_folder=TEMPLATES_DIR)
 
+SECRET_KEY = os.urandom(32)
+app.config["SECRET_KEY"] = SECRET_KEY
+
+
+class PdfFileField(wtforms.FileField):
+    def __call__(self, *args, **kwargs):
+        # Set the accept attribute to only allow PDF files
+        kwargs.setdefault("accept", "*.pdf")
+        return super().__call__(*args, **kwargs)
+
+
+class InvertPdfForm(flask_wtf.FlaskForm):
+    file = PdfFileField(
+        "PDF File",
+        validators=[
+            wtforms.validators.DataRequired(),
+            wtforms.validators.regexp(r".*\.pdf$", message="File must be a PDF"),
+        ],
+    )
+    submit = wtforms.SubmitField("Invert PDF")
+
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html.j2")
+    form = InvertPdfForm()
+    return flask.render_template("index.html.j2", form=form)
 
 
 if __name__ == "__main__":
